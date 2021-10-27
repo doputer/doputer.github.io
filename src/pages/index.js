0aee7c6acbfe797ec43046409d37b08b45cd8992
Dohyeon <swputer@gmail.com>
2021-10-27 23:03:28 +0900

    feat: 스타일 색상 변경

--- src/pages/index.js
+++ src/pages/index.js
@@ -79,7 +79,7 @@ export const pageQuery = graphql`
     ) {
       edges {
         node {
-          excerpt(pruneLength: 200, truncate: true)
+          excerpt(pruneLength: 160, truncate: true)
           fields {
             slug
           }
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -6,7 +6,7 @@ GitHubTheme.overrideThemeStyles = () => {
     a: {
       boxShadow: `none`,
       textDecoration: `none`,
-      color: `#3372dc`,
+      color: `#4361ee`,
     },
 
     'a.gatsby-resp-image-link': {
