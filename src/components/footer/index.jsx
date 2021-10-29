0a477fcc04725d609d6e1d35e004cfeac05f3267
Dohyeon <swputer@gmail.com>
2021-10-29 20:40:40 +0900

    feat: 푸터 링크 수정

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,8 +4,12 @@ import './index.scss'
 
 export const Footer = () => (
   <footer className="footer">
-    ©<a href="https://github.com/JaeYeopHan">Doputer</a>, Built with{' '}
-    <a href="https://github.com/JaeYeopHan/gatsby-starter-bee">
+    ©
+    <a href="https://github.com/doputer" target="_blank">
+      Doputer
+    </a>
+    , Built with{' '}
+    <a href="https://github.com/JaeYeopHan/gatsby-starter-bee" target="_blank">
       Gatsby-starter-bee
     </a>
   </footer>
