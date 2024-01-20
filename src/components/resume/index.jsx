873d392519e5dad14fb2ba20d868e71c1ed0d4ff
김도현 <swputer@gmail.com>
2024-01-21 00:17:01 +0900

    chore: 기존 파일 제거

--- src/components/resume/index.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-import './index.scss'
-
-export const Resume = ({ children }) => <div className="resume">{children}</div>
--- src/components/tag-container/index.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-import './index.scss'
-
-export const TagContainer = React.memo(({ children }) => (
-  <ul className="tag-container">{children}</ul>
-))
