f2c8ce04f83c6d329d0fdca51c8585470d6c82bb
김도현 <swputer@gmail.com>
2024-01-23 00:18:04 +0900

    feat: 타이포그래피 제거

--- src/components/typography/code.tsx
+++
@@ -1,5 +0,0 @@
-function code(props: React.HtmlHTMLAttributes<HTMLElement>) {
-  return <code className="bg-muted rounded px-[0.3rem] py-[0.2rem] text-sm" {...props} />;
-}
-
-export default code;
--- src/components/typography/index.tsx
+++ src/components/typography/index.tsx
@@ -1,8 +1,7 @@
 import a from '@/components/typography/a';
-import code from '@/components/typography/code';
 import h2 from '@/components/typography/h2';
 import img from '@/components/typography/img';
 import ol from '@/components/typography/ol';
 import p from '@/components/typography/p';
 
-export default { a, code, h2, img, ol, p };
+export default { a, h2, img, ol, p };
