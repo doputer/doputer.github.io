5a121d97ef5cf446639cc5965e90a5c6a917da0f
김도현 <swputer@gmail.com>
2024-01-27 06:23:46 +0900

    feat: 푸터 추가

---
+++ src/components/footer.tsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer className="flex justify-center">
+      <a
+        className="text-link"
+        href="https://github.com/doputer"
+        target="_blank"
+        rel="noreferrer"
+        aria-label="github_anchor"
+      >
+        GitHub
+      </a>
+    </footer>
+  );
+}
+
+export default Footer;
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from '@/components/footer';
 import Header from '@/components/header';
 
 interface LayoutProps {
@@ -6,10 +7,11 @@ interface LayoutProps {
 
 function Layout({ children }: LayoutProps) {
   return (
-    <div className="min-h-screen w-screen px-8 pt-8 text-dark transition-colors dark:bg-dark dark:text-light">
+    <div className="min-h-screen w-screen p-8 text-dark transition-colors dark:bg-dark dark:text-light">
       <div className="mx-auto my-0 max-w-2xl">
         <Header />
-        <main className="mt-8 pb-8">{children}</main>
+        <main className="my-8">{children}</main>
+        <Footer />
       </div>
     </div>
   );
