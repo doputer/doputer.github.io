32cf7e0a148e1bbc606bcdcec3d3a651b86d8993
김도현 <swputer@gmail.com>
2024-02-03 09:26:24 +0900

    refactor: 리스트 컴포넌트 추가
    - 공통 컴포넌트로 분리

--- src/templates/page.tsx
+++ src/templates/page.tsx
@@ -1,6 +1,7 @@
-import { graphql, Link, type PageProps } from 'gatsby';
+import { graphql, type PageProps } from 'gatsby';
 
 import Counter from '@/components/counter';
+import List from '@/components/list';
 import Pagination from '@/components/pagination';
 import SEO from '@/components/seo';
 
@@ -11,27 +12,7 @@ function PageTemplate({
   return (
     <div className="flex flex-col gap-8">
       <Counter count={allMdx.totalCount} />
-      {allMdx.nodes.map(({ fields, frontmatter }, index) => (
-        <div key={index} className="group flex flex-wrap items-center justify-center gap-4">
-          <div className="flex w-full items-center justify-center rounded-lg bg-background-light p-12 text-6xl dark:bg-background-dark xs:w-fit">
-            <div className="group-hover:animate-flip">{frontmatter.emoji}</div>
-          </div>
-          <div className="flex-1">
-            <Link to={fields.slug} className="text-2xl font-semibold">
-              {frontmatter.title}
-            </Link>
-            <div className="flex gap-2 text-sm text-link-light dark:text-link-dark">
-              {frontmatter.tags.map((tag) => (
-                <span key={tag}>{tag.toUpperCase()}</span>
-              ))}
-            </div>
-            <div className="my-4 text-mute-light dark:text-mute-dark">
-              {frontmatter.description}
-            </div>
-            <time>{frontmatter.date}</time>
-          </div>
-        </div>
-      ))}
+      <List allMdx={allMdx} />
       <Pagination currentPage={currentPage} />
     </div>
   );
--- src/templates/tag.tsx
+++ src/templates/tag.tsx
@@ -1,6 +1,7 @@
-import { graphql, Link, type PageProps } from 'gatsby';
+import { graphql, type PageProps } from 'gatsby';
 
 import Counter from '@/components/counter';
+import List from '@/components/list';
 import SEO from '@/components/seo';
 
 function TagTemplate({
@@ -10,27 +11,7 @@ function TagTemplate({
   return (
     <div className="flex flex-col gap-8">
       <Counter label={tag} count={totalCount} />
-      {allMdx.nodes.map(({ fields, frontmatter }, index) => (
-        <div key={index} className="group flex flex-wrap items-center justify-center gap-4">
-          <div className="flex w-full items-center justify-center rounded-lg bg-background-light p-12 text-6xl dark:bg-background-dark xs:w-fit">
-            <div className="group-hover:animate-flip">{frontmatter.emoji}</div>
-          </div>
-          <div className="flex-1">
-            <Link to={fields.slug} className="text-2xl font-semibold">
-              {frontmatter.title}
-            </Link>
-            <div className="flex gap-2 text-sm text-link-light dark:text-link-dark">
-              {frontmatter.tags.map((tag) => (
-                <span key={tag}>{tag.toUpperCase()}</span>
-              ))}
-            </div>
-            <div className="my-4 text-mute-light dark:text-mute-dark">
-              {frontmatter.description}
-            </div>
-            <time>{frontmatter.date}</time>
-          </div>
-        </div>
-      ))}
+      <List allMdx={allMdx} />
     </div>
   );
 }
