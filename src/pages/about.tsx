c77a5aef2c2ae991a1931cbc807bcb35e6af6135
김도현 <swputer@gmail.com>
2024-02-06 00:59:24 +0900

    feat: 어바웃 페이지 추가

---
+++ src/pages/about.tsx
@@ -0,0 +1,34 @@
+import SEO from '@/components/seo';
+
+function AboutPage() {
+  return (
+    <>
+      <p className="mb-8 animate-flip rounded-full text-center text-6xl">🔥</p>
+      <p className="mb-8 text-center text-lg">안녕하세요. 개발자 김도현 입니다.</p>
+      <p className="flex justify-center gap-8 text-center text-lg">
+        <a
+          className="text-link-light dark:text-link-dark"
+          href="https://github.com/doputer"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="github_anchor"
+        >
+          GitHub
+        </a>
+        <a
+          className="text-link-light dark:text-link-dark"
+          href="mailto:swputer@gmail.com"
+          target="_blank"
+          rel="noreferrer"
+          aria-label="github_anchor"
+        >
+          Email
+        </a>
+      </p>
+    </>
+  );
+}
+
+export default AboutPage;
+
+export const Head = () => <SEO />;
