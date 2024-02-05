def6f4708e85ef3d41624531f66fc88ccc598c67
김도현 <swputer@gmail.com>
2024-02-06 00:27:50 +0900

    chore: 함수 이름 수정

--- src/pages/tags.tsx
+++ src/pages/tags.tsx
@@ -3,7 +3,7 @@ import { graphql, Link, PageProps } from 'gatsby';
 import Counter from '@/components/counter';
 import SEO from '@/components/seo';
 
-function Tags({ data: { allMdx } }: PageProps<Queries.TagsQuery>) {
+function TagsPage({ data: { allMdx } }: PageProps<Queries.TagsQuery>) {
   return (
     <div className="flex flex-col gap-8">
       <Counter label="tags" count={allMdx.tags.length} />
@@ -22,7 +22,7 @@ function Tags({ data: { allMdx } }: PageProps<Queries.TagsQuery>) {
   );
 }
 
-export default Tags;
+export default TagsPage;
 
 export const query = graphql`
   query Tags {
