e081b67452663cb1d22f37168506408db964c6f4
김도현 <swputer@gmail.com>
2024-02-13 22:37:28 +0900

    feat: 타이포그래피 수정

--- src/components/typography/strong.tsx
+++ src/components/typography/strong.tsx
@@ -1,5 +1,5 @@
 function strong(props: React.HTMLAttributes<HTMLElement>) {
-  return <strong className="font-bold" {...props} />;
+  return <strong className="font-semibold" {...props} />;
 }
 
 export default strong;
