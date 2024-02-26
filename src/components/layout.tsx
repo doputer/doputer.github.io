16754b287028d4aad475060712444e19bca034b2
김도현 <swputer@gmail.com>
2024-02-27 01:56:28 +0900

    design: 좌우 padding 수정

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -6,7 +6,7 @@ interface LayoutProps {
 
 function Layout({ children }: LayoutProps) {
   return (
-    <div className="min-h-screen p-8 text-dark transition-colors dark:bg-dark dark:text-light">
+    <div className="min-h-screen px-4 py-8 text-dark transition-colors dark:bg-dark dark:text-light">
       <div className="mx-auto max-w-2xl space-y-8">
         <Slice alias="header" />
         <main className="space-y-8">{children}</main>
