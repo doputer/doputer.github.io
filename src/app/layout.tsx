9a0fd3f49e43051553ffd2991d819c84264363b9
김도현 <swputer@gmail.com>
2024-11-04 00:17:58 +0900

    feat: footer 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import '@/app/globals.css';
 
+import Footer from '@/components/footer';
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -9,6 +11,7 @@ export default function RootLayout({
     <html>
       <body className="min-h-screen px-4 py-8 text-dark transition-colors dark:bg-dark dark:text-light">
         <main className="mx-auto max-w-2xl space-y-8">{children}</main>
+        <Footer />
       </body>
     </html>
   );
