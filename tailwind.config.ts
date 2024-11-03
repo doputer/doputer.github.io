3fce648e1bca07cbc441baa41428a282cac3ce62
김도현 <swputer@gmail.com>
2024-11-03 21:47:46 +0900

    build: Next.js 설정

---
+++ tailwind.config.ts
@@ -0,0 +1,54 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: ['./src/components/**/*.{js,ts,jsx,tsx,mdx}', './src/app/**/*.{js,ts,jsx,tsx,mdx}'],
+  theme: {
+    fontFamily: {
+      sans: ['Pretendard', 'sans-serif'],
+      mono: ['JetBrainsMono', 'consolas'],
+    },
+    extend: {
+      colors: {
+        light: {
+          DEFAULT: '#dddddd',
+          mute: '#595959',
+          link: '#004ec7',
+          background: '#eef1f5',
+          line: '#e5e7eb',
+          code: {
+            DEFAULT: '#ea580c',
+            background: '#fb923c1a',
+          },
+        },
+        dark: {
+          DEFAULT: '#191c1f',
+          mute: '#b3b3b3',
+          link: '#10b981',
+          background: '#222527',
+          line: '#3a3e42',
+          code: {
+            DEFAULT: '#a78bfa',
+            background: '#f3e8ff1a',
+          },
+        },
+        dimmed: '#00000066',
+      },
+      screens: {
+        xs: '480px',
+      },
+      animation: {
+        flip: 'flip 1s linear infinite',
+      },
+      keyframes: {
+        flip: {
+          '0%': { transform: 'rotateY(0)' },
+          '100%': { transform: 'rotateY(360deg)' },
+        },
+      },
+    },
+  },
+  darkMode: 'selector',
+  plugins: [],
+} satisfies Config;
+
+export default config;
