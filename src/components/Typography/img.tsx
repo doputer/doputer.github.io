137db1e738152672e6fab10b7153561d84c6ee12
김도현 <swputer@gmail.com>
2024-11-10 01:02:01 +0900

    feat: 이미지 개선

--- src/components/Typography/img.tsx
+++ src/components/Typography/img.tsx
@@ -1,5 +1,22 @@
-function img(props: React.ImgHTMLAttributes<HTMLImageElement>) {
-  return <img className="m-auto my-4" {...props} />;
+import Image from 'next/image';
+
+type ImageProps = React.HTMLAttributes<HTMLImageElement> & {
+  src?: string;
+};
+
+function img(props: Omit<ImageProps, 'alt'>) {
+  return (
+    <Image
+      className="m-auto my-4 h-auto w-fit"
+      width={0}
+      height={0}
+      sizes="100vw"
+      loading="eager"
+      src={props.src as string}
+      alt=""
+      {...props}
+    />
+  );
 }
 
 export default img;
