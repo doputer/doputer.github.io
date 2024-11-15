e945a29507ddb258e764cba43f7771097f69a813
김도현 <swputer@gmail.com>
2024-11-16 01:30:53 +0900

    refactor: scroll 개선

--- src/components/Typography/h3.tsx
+++ src/components/Typography/h3.tsx
@@ -1,5 +1,5 @@
 function h3(props: React.HTMLAttributes<HTMLHeadingElement>) {
-  return <h3 className="mb-4 mt-8 text-2xl font-semibold" {...props} />;
+  return <h3 className="mb-4 mt-8 scroll-mt-4 text-2xl font-semibold" {...props} />;
 }
 
 export default h3;
