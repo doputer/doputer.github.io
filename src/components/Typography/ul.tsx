62fb14aabcfae0b99b017b21111d822ccb316b0b
김도현 <swputer@gmail.com>
2024-11-16 05:52:11 +0900

    fix: 중첩 리스트 스타일

--- src/components/Typography/ul.tsx
+++ src/components/Typography/ul.tsx
@@ -1,5 +1,10 @@
 function ul(props: React.HTMLAttributes<HTMLUListElement>) {
-  return <ul className="my-4 list-inside list-disc leading-8 [&_ul]:my-0 [&_ul]:ml-4" {...props} />;
+  return (
+    <ul
+      className="my-4 list-inside list-disc leading-8 [&_ol]:my-0 [&_ol]:ml-4 [&_ul]:my-0 [&_ul]:ml-4"
+      {...props}
+    />
+  );
 }
 
 export default ul;
