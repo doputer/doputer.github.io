211a07b67aedb8e01d7b74e9c71b9c6eb16cc624
김도현 <swputer@gmail.com>
2024-11-18 03:16:36 +0900

    fix: Code 컴포넌트 스타일 수정

--- src/components/Typography/format.ts
+++ src/components/Typography/format.ts
@@ -17,5 +17,5 @@ export const th = 'p-2';
 export const td = 'p-2';
 export const a = 'font-medium text-secondary underline underline-offset-4';
 export const strong = 'font-semibold';
-export const pre = '!rounded-lg border border-line font-mono text-sm leading-6';
+export const pre = '!rounded-lg border border-line font-mono text-sm leading-6 [&_*]:z-0';
 export const img = 'm-auto my-4 h-auto w-fit rounded';
