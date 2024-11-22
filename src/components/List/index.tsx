d2bf4e23fdc3b255dab0d26e05648bf32d618bee
김도현 <swputer@gmail.com>
2024-11-23 02:52:06 +0900

    feat: 리스트 컴포넌트 수정

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 
-import { Post } from '@/lib/MDX/types';
+import type { Post } from '@/lib/MDX/types';
 
 interface ListProps {
   posts: Post[];
@@ -12,26 +12,26 @@ const List = async ({ posts }: ListProps) => {
       {posts.map(({ frontmatter, slug }) => (
         <div
           key={frontmatter.title}
-          className="group flex flex-wrap items-center justify-center gap-4"
+          className="group relative flex flex-wrap items-center justify-center gap-4 py-4 before:absolute before:inset-0 before:-z-10 before:hidden before:scale-x-110 before:rounded-lg hover:before:bg-surface xs:gap-8 xs:before:block"
         >
-          <div className="flex w-full items-center justify-center rounded-lg bg-surface p-10 text-6xl xs:w-fit">
-            <div className="pointer-events-none select-none group-hover:animate-flip">
+          <div className="flex w-full items-center justify-center rounded-lg bg-surface p-10 xs:w-fit xs:bg-inherit xs:p-0">
+            <div className="pointer-events-none select-none text-5xl group-hover:animate-flip">
               {frontmatter.emoji}
             </div>
           </div>
           <div className="flex-1">
-            <Link href={`/${slug}`} className="text-2xl font-semibold">
+            <Link href={`/${slug}`} className="mr-2 text-2xl font-medium">
               {frontmatter.title}
             </Link>
-            <div className="flex flex-wrap gap-2 text-sm uppercase text-secondary">
+            <time className="text-sm font-light">{frontmatter.date}</time>
+            <div className="space-x-2 text-sm uppercase text-secondary">
               {frontmatter.tags.map((tag) => (
                 <Link key={tag} href={`/tags/${tag.replace(/\s/g, '-')}`}>
                   {tag}
                 </Link>
               ))}
             </div>
-            <div className="my-2 text-muted">{frontmatter.description}</div>
-            <time>{frontmatter.date}</time>
+            <div className="mt-2 text-muted">{frontmatter.description}</div>
           </div>
         </div>
       ))}
