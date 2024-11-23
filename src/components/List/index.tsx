62398fa4735aac2b807943648a601a871918475e
김도현 <swputer@gmail.com>
2024-11-23 15:56:54 +0900

    feat: 리스트 컴포넌트 수정

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -7,36 +7,39 @@ interface ListProps {
   posts: Post[];
 }
 
-const List = async ({ posts }: ListProps) => {
+const List = ({ posts }: ListProps) => {
   return (
-    <>
+    <div className="divide-y divide-dashed divide-line">
       {posts.map(({ frontmatter, slug }) => (
         <div
           key={frontmatter.title}
-          className="group relative flex flex-wrap items-center justify-center gap-4 py-0 before:absolute before:inset-0 before:-z-10 before:hidden before:scale-x-110 before:rounded-lg hover:before:bg-surface xs:gap-8 xs:py-4 xs:before:block"
+          className="[&:not(:first-of-type)]:pt-4 [&:not(:last-of-type)]:pb-4"
         >
-          <div className="pointer-events-none select-none text-3xl group-hover:animate-flip xs:text-5xl">
-            {frontmatter.emoji}
-          </div>
-          <div className="flex-1">
-            <div className="flex items-baseline justify-between gap-2 xs:justify-normal">
-              <Link href={`/${slug}`} className="break-keep text-base font-medium xs:text-2xl">
-                {frontmatter.title}
-              </Link>
-              <time className="text-xs font-light xs:text-sm">{frontmatter.date}</time>
+          <div className="group flex items-center gap-8 rounded-lg px-0 py-2 md:px-6 md:py-4 md:hover:bg-surface">
+            <div className="pointer-events-none hidden select-none text-5xl group-hover:animate-flip md:block">
+              {frontmatter.emoji}
             </div>
-            <div className="space-x-2 text-xs uppercase text-secondary xs:text-sm">
-              {frontmatter.tags.map((tag) => (
-                <Link key={tag} href={`/tags/${encode(tag)}`}>
-                  {tag}
+            <div className="flex-1">
+              <div className="flex items-baseline justify-between gap-2 md:justify-normal">
+                <Link href={`/${slug}`} className="break-keep text-base font-medium md:text-2xl">
+                  <span className="mr-2 md:hidden">{frontmatter.emoji}</span>
+                  {frontmatter.title}
                 </Link>
-              ))}
+                <time className="text-xs font-light md:text-sm">{frontmatter.date}</time>
+              </div>
+              <div className="space-x-2 text-xs uppercase text-secondary md:text-sm">
+                {frontmatter.tags.map((tag) => (
+                  <Link key={tag} href={`/tags/${encode(tag)}`}>
+                    {tag}
+                  </Link>
+                ))}
+              </div>
+              <div className="mt-2 text-sm text-muted md:text-base">{frontmatter.description}</div>
             </div>
-            <div className="mt-2 text-sm text-muted xs:text-base">{frontmatter.description}</div>
           </div>
         </div>
       ))}
-    </>
+    </div>
   );
 };
 
