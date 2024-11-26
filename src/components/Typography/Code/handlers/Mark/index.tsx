b6bfac64840fa69d59668ccb4754dffbfaeb5c75
김도현 <swputer@gmail.com>
2024-11-27 01:39:08 +0900

    docs: 포스트 수정

--- src/components/Typography/Code/handlers/Mark/index.tsx
+++ src/components/Typography/Code/handlers/Mark/index.tsx
@@ -2,12 +2,8 @@ import { type AnnotationHandler, InnerLine } from 'codehike/code';
 
 const mark: AnnotationHandler = {
   name: 'mark',
-  Line: (props) => {
-    return <InnerLine merge={props} className="px-4" />;
-  },
-  AnnotatedLine: (props) => {
-    return <InnerLine merge={props} className="bg-mark px-4" />;
-  },
+  Line: (props) => <InnerLine merge={props} className="px-4" />,
+  AnnotatedLine: (props) => <InnerLine merge={props} className="bg-mark px-4" />,
 };
 
 export default mark;
